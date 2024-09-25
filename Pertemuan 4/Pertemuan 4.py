0a05f92a9bab3e9df67dee6c4f86bbf9c2f67565
Adityamr2005 <adityamahyudiramadhan403@gmail.com>
2024-09-25 16:15:21 +0800

    Pertemuan 4

---
+++ Pertemuan 4/Pertemuan 4.py
@@ -0,0 +1,49 @@
+#ulang = 10
+#for i in range(ulang):
+#    print("halo")
+    
+#simpan = [12, "udin petot", 14.5, True, 'A', "102"]
+#for i in simpan:
+#    print(i)
+
+#makanan = ["mie","sop","bakso"]
+#minuman = ["es teh","air putih","es jeruk"]
+
+#for i in makanan:
+#   for j in minuman:
+#        print(f"[i] & [j]")
+        
+#print("Menu Rumah Makan Informatika 24: ")
+#print("--------------------------------")
+#simpan = ["Nasi Goreng", "Mie Goreng", "Mie Ayam", "Bakso", "Soto"]
+#for i in simpan:
+#    print(i)
+    
+#print("Menu Rumah Makan Informatika 24: ")
+#print("--------------------------------")
+#simpan = ["Nasi Goreng", "Mie Goreng", "Mie Ayam", "Bakso", "Soto"]
+#for i, menu in enumerate(simpan,start=100):
+#    print(f"{i}.{menu}")
+    
+#print("Menu Rumah Makan Informatika 24: ")
+#print("--------------------------------")
+#simpan = ["Nasi Goreng", "Mie Goreng", "Mie Ayam", "Bakso", "Soto"]
+#for i in range(len(simpan)):
+#    print(f"{i+1}. {simpan[i]}")
+    
+#hitung = 0
+#while True:
+#    hitung += 1
+#    ulang = input("masih ingin lanjut?")
+#    if ulang == "ya" or ulang == "Y":
+#        print("oke kita lanjut")
+#        continue
+
+hitung = +0
+while True:
+    hitung += -1
+    ulang = input("anda masih berlanjut?")
+    if ulang == "Tidak" or ulang == "G":
+        print("baiklah anda tidak melanjutkan")
+        
+        
