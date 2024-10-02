ef02975a75f380a1811d1a767c4f775cb505d648
Adityamr2005 <adityamahyudiramadhan403@gmail.com>
2024-10-02 16:45:02 +0800

    Pertemuan 5

---
+++ Pertemuan 5/Pertemuan 5.py
@@ -0,0 +1,58 @@
+#nama = ["shandy",106,True,
+#        ["yuyun",145],3.96,
+#        [123, "ALVITO",False,3,66],
+#        "rehan"]
+#print(nama[4])
+
+#matkul = ["APD", 
+#          "APL", 
+#          "WEB", 
+#          "jarkom", 
+#          "BASDAT", 
+#          "STRUKDAT", 
+#          "PTI", 
+#          "KALKULUS", 
+#          "PROBAS"
+#]
+#print(matkul[8])
+
+#makanan = ["Bakso","Sate","Soto","Nasi Goreng","Mie Ayam","Ayam Bakar","Cumi Bakar"]
+#print("Sebelum: ")
+#print(makanan)
+#makanan.append("Nasi Goreng")
+#del makanan[5]
+#print("Sesudah: ")
+#makanan.clear()
+#print(makanan)
+#data = makanan.pop(5)
+#print(makanan)
+#print(data)
+#print(makanan)
+#makanan.insert(1,"Nasi Goreng")
+#makanan[1] = "Nasi Goreng Pattaya"
+#print(makanan)
+
+
+#minuman 6. 3(dihapus) 6(air putih) 0(jus tomat)
+#minuman = ["Marjan","Coca Cola","Bear Brand","Pop Ice Taro","Teh Pucuk Harum","Thai Tea"]
+#print("sebelum")
+#print(minuman)
+#del minuman[2]
+#print("sesudah")
+#print(minuman)
+#minuman[4] = "Aqua"
+#print(minuman)
+#minuman.insert(0,"Jus Tomat")
+#print(minuman)
+
+#makanan = ["Ayam","Ikan",["Bakso","Soto","Sate","Ikan","Bebek"],
+#           ["Teh","Kopi","Air"]]
+
+#for i in makanan :
+#    if isinstance(i, list): 
+#        for j in i :
+#            print(j)
+#    else:
+#        print(i)
+
+    
